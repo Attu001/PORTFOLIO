b147ae6201fe0961a9843b489aedd2f6c192544b
Attu001 <atishchavan066@gmail.com>
2025-03-14 08:34:52 +0530

    added new project to the porfolio

--- src/Componentes/Workdata.js
+++ src/Componentes/Workdata.js
@@ -2,6 +2,9 @@ import pro1 from '../assets/Shoper-head.png'
 import pro2 from '../assets/Tomato-head.png'
 
 import pro3 from '../assets/Harborlight-head.png'
+import pro4 from '../assets/online_shopping_project-head.png'
+import pro5 from '../assets/Ebook-head.png'
+import pro6 from '../assets/Yolo-head.webp'
 
 const ProjectCardData=[
 
@@ -25,7 +28,29 @@ const ProjectCardData=[
             title:"HarborLight" ,
             text:"Hotel website.",
             view:"https://attu001.github.io/HarborLights/"
-        }     
+        },
+        {
+            id:4,
+            imgsrc:pro4,
+            title:"Ebook_management_system" ,
+            text:"📚 Ebook Management System Welcome to the Ebook Management System repository! This project is developed using Java, JDBC, JSP, JSTL, and MySQL to manage a collection of ebooks with various functionalities.",
+            view:"https://github.com/Attu001/Ebook_Management"
+        },
+        {
+            id:5,
+            imgsrc:pro5,
+            title:"Ecommerce" ,
+            text:"🛒 Online Shopping Web Application OverviewThis project is a modern web application for online shopping, providing users with the ability to browse and purchase products. It features user authentication, product listings, shopping cart management, and order processing.",
+            view:"https://github.com/Attu001/Online_shopping_project"
+        },
+        {
+            id:6,
+            imgsrc:pro6,
+            title:"Yolo_Object_Detection" ,
+            text:"This project demonstrates object detection using the YOLOv8 model from the Ultralytics library. The script loads a pretrained YOLOv8 model, processes an image, and detects objects by drawing bounding boxes around them",
+            view:"https://github.com/Attu001/Yolo_Object_Detection/tree/master"
+        }
+
 
 ];
 
