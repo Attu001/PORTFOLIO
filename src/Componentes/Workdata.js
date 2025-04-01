c3c01ac5cd9cd966c8d9e4377c1360486d56be8a
Attu001 <atishchavan066@gmail.com>
2025-04-01 07:38:14 +0530

    Resolved merge conflict in Workdata.js

--- src/Componentes/Workdata.js
+++ src/Componentes/Workdata.js
@@ -2,6 +2,9 @@ import pro1 from '../assets/Shoper-head.png'
 import pro2 from '../assets/Tomato-head.png'
 
 import pro3 from '../assets/Harborlight-head.png'
+import pro4 from '../assets/online_shopping_project-head.png'
+import pro5 from '../assets/Ebook-head.png'
+import pro6 from '../assets/Yolo-head.webp'
 
 import Ebook from '../assets/Ebook.png'
 import onlineshopping from '../assets/online_shopping_project.png'
@@ -36,13 +39,19 @@ const ProjectCardData=[
         },
         {
             id:4,
+<<<<<<< HEAD
             imgsrc:Ebook,
             title:"Ebook_management_system",
+=======
+            imgsrc:pro4,
+            title:"Ebook_management_system" ,
+>>>>>>> b147ae6201fe0961a9843b489aedd2f6c192544b
             text:"📚 Ebook Management System Welcome to the Ebook Management System repository! This project is developed using Java, JDBC, JSP, JSTL, and MySQL to manage a collection of ebooks with various functionalities.",
             view:"https://github.com/Attu001/Ebook_Management"
         },
         {
             id:5,
+<<<<<<< HEAD
             imgsrc:onlineshopping,
             title:"Online Shopping Project",
             text:"🛒 Online Shopping Web Application Overview This project is a modern web application for online shopping, providing users with the ability to browse and purchase products. It features user authentication, product listings, shopping cart management, and order processing.",
@@ -58,6 +67,21 @@ const ProjectCardData=[
             source:"https://github.com/Attu001/Yolo_Object_Detection/tree/master"
         }
           
+=======
+            imgsrc:pro5,
+            title:"Ecommerce" ,
+            text:"🛒 Online Shopping Web Application OverviewThis project is a modern web application for online shopping, providing users with the ability to browse and purchase products. It features user authentication, product listings, shopping cart management, and order processing.",
+            view:"https://github.com/Attu001/Online_shopping_project"
+        },
+        {
+            id:6,
+            imgsrc:pro6,
+            title:"Yolo_Object_Detection" ,
+            text:"This project demonstrates object detection using the YOLOv8 model from the Ultralytics library. The script loads a pretrained YOLOv8 model, processes an image, and detects objects by drawing bounding boxes around them",
+            view:"https://github.com/Attu001/Yolo_Object_Detection/tree/master"
+        }
+
+>>>>>>> b147ae6201fe0961a9843b489aedd2f6c192544b
 
 ];
 
